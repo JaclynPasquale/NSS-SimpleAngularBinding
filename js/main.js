c2b4e096d38feccd6cbb29a8d6e9900106e58ce1
jaclynpasquale <jaclynpasquale@gmail.com>
2014-12-04 14:40:06 -0600

    finished friends name app

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-;(function(){
+;
+(function() {
   'use strict';
 
-  var hello = 'world';  
+  angular.module('myApp', [])
+    .controller('SimpleController', function() {
+      //$scope.name = "Scott"; using this is the preferred method
+      var vm = this;
+      vm.name = "Scott";
+      vm.friends = ['Ke$ha', 'LadyGaga', 'Shaniqua', 'Kimbra',
+        'Kashira', 'D\'andre'
+      ];
+      //$scope.friends = ['Ke$ha', 'LadyGaga', 'Shaniqua', 'Kimbra',
+      //  'Kashira', 'D\'andre'];
+    });
+
+
 }());
